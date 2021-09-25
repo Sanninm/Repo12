ae30d1cd081018de2e3db70d8aeb51dba360aaf9
Sanninm <mudzelet.sanin@gmail.com>
2021-09-25 10:36:03 +0200

    ex04

--- ex04/src/index.js
+++ ex04/src/index.js
@@ -1,16 +1,23 @@
 // Only change code below this line 
 function sumFibonacci(num) {
-    if (num===1) {
-    return (0, 1);
+    
+ if (num < 0) {
+    return [0, 1];
 }
  else {
-    
-    return sumFibonacci(num-1) + sumFibonacci(num-2);
-   
-}   
+    var array = sumFibonacci(num-1)
+    if((array[array.length-1])<num) {
+        array.push(array[array.length-1]+array[array.length-2])
 }
+return array;
+}
+} 
+
 // Only change code above this line 
 
 console.log(sumFibonacci(1));
 console.log(sumFibonacci(10));
+console.log(sumFibonacci(20));
+console.log(sumFibonacci(4));
+console.log(sumFibonacci(-5));
 module.exports = sumFibonacci;
